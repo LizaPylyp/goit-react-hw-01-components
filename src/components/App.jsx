12de460d3e60e9e685fff7b1dd0de7c3cad30b2e
Liza <lizkapylypchuk123456@gmail.com>
2023-08-01 21:50:25 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,40 @@
+import { Profile } from './Profile/profile'
+import user from './Profile/user.json'
+
+import { Statistics } from './Statistics/statistics'
+import data from './Statistics/data.json'
+
+import { FriendList } from './FriendList/friendsData'
+import friends from './FriendList/friendsArr.json'
+
+import { TransactionHistory } from './TransactionHistory/transactionhistory'
+import transactions from './TransactionHistory/transaction.json'
+
 export const App = () => {
   return (
     <div
       style={{
         height: '100vh',
-        display: 'flex',
+        // display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
-        fontSize: 40,
+        fontSize: 20,
         color: '#010101'
       }}
-    >
-      React homework template
+      >
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        followers={user.stats.followers}
+        views={user.stats.views}
+        likes={user.stats.likes}
+      />
+      <Statistics title="Upload stats" data={data} />
+      <FriendList friends = {friends}  />
+      <TransactionHistory transactions={transactions} /> 
     </div>
   );
 };
+
